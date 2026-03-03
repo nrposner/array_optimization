cd9180b75d36dc0c21fb7169cee55e21be6f34b5
nrposner <nicolasposner@gmail.com>
2026-03-03 16:07:36 -0500

    updating benchmarks and notebook

--- python/benchmark.py
+++ python/benchmark.py
@@ -9,6 +9,8 @@ import matplotlib.pyplot as plt
 
 SLOW = False
 
+magnitudes = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000]
+
 def numpy_array(a, b, c, d):
     return ((a*b)/c)**d
 
@@ -201,7 +203,7 @@ def plot_results(all_mean_times):
     plt.show()
 
 all_mean_times = []
-for i in [10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000]:
+for i in magnitudes:
     all_mean_times.append((i, bench(i)))
 
 plot_results(all_mean_times)
